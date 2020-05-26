e67a8f1487c3b9ec49b6e4e62428c57fdd4df2d0
Mawathilde <mawathilde@gmail.com>
2020-05-26 21:48:51 +0200

    Initial commit

---
+++ src/main/java/fr/mawathilde/structuremover/StructureMover.java
@@ -0,0 +1,11 @@
+package fr.mawathilde.structuremover;
+
+import fr.mawathilde.structuremover.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID)
+public class StructureMover {
+
+
+
+}
---
+++ src/main/java/fr/mawathilde/structuremover/util/Reference.java
@@ -0,0 +1,7 @@
+package fr.mawathilde.structuremover.util;
+
+public final class Reference {
+
+    public static final String MOD_ID = "sm";
+
+}
